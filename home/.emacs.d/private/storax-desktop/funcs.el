617e6535bbf11a5cfcc5f54c36004bbf0fefc840
David Zuber <zuber.david@gmx.de>
2016-02-21 15:38:47 +0000

    Add icons

---
+++ home/.emacs.d/private/storax-desktop/funcs.el
@@ -0,0 +1,23 @@
+;;; funcs.el --- storax-desktop layer functions file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: David Zuber <zuber.david@gmx.de>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defun storax/desktop-save ()
+  "Save the current desktop to desktop-dirname."
+  (interactive)
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
+
+;;; functs.el ends here
