d23c35bddc8d43429339baacf64b7ff595421ee4
David Zuber <zuber.david@gmx.de>
2016-03-21 22:45:13 +0000

    Add erc config

--- home/.spacemacs
+++ home/.spacemacs
@@ -109,6 +109,7 @@ values."
      ;; storax-powerline
      storax-compilation
      storax-semantic
+     storax-erc
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
---
+++ home/.spacemacs.d/private/storax-erc/packages.el
@@ -0,0 +1,58 @@
+;;; packages.el --- storax-erc layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: David Zuber <zuber.david@gmx.de>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(configuration-layer/declare-layer 'storax-secret)
+
+(defconst storax-erc-packages
+  '(erc))
+
+
+(defun storax-erc/post-init-erc ()
+  (use-package erc
+    :config
+    (storax/load-secrets)
+    (setq erc-nick gitter-username
+          erc-password gitter-token
+          erc-server "irc.gitter.im"
+          erc-modules
+          '(autojoin
+            button
+            completion
+            fill
+            hl-nicks
+            image
+            irccontrols
+            list
+            log
+            match
+            menu
+            move-to-prompt
+            netsplit
+            networks
+            noncommands
+            pcomplete
+            readonly
+            ring
+            services
+            stamp
+            track
+            youtube)
+          erc-autojoin-channels-alist
+          '(("irc.gitter.im" . ("signalpiller"
+                                "magit/magit"
+                                "syl20bnr/spacemacs"))))))
+
+
+;;; packages.el ends here
