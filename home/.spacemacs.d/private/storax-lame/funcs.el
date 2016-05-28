a5330319e14502659f17a016b795d4b7d0c05b44
David Zuber <zuber.david@gmx.de>
2016-05-28 16:02:30 +0100

    More lame macros

--- home/.spacemacs.d/private/storax-lame/funcs.el
+++ home/.spacemacs.d/private/storax-lame/funcs.el
@@ -20,7 +20,10 @@
   "The current task.")
 
 (defvar lame-task-buffer nil
-  "The curren task buffer.")
+  "The current task buffer.")
+
+(defvar lame-edit-buffer nil
+  "The current edit buffer.")
 
 (defun lame/interactive-args (func)
   "Get the interactive args of FUNC."
@@ -40,14 +43,14 @@ SYMBOLS should be a list the length of the interactive args of FUNC.
             ,@body)))
 
 (defmacro lame/task (taskname description &rest body)
-  "Create a task with TASKNAME and DESCRIPTION and execute BODY."
+  "Create a task with TASKNAME and DESCRIPTION and execute BODY.
+The task is marked as finished after calling `lame/done'."
   `(let ((taskbuffername (format "Task: %s" ,taskname)))
      (when (or (not lame-task) (y-or-n-p "Cancel currently running task?"))
-       (display-buffer taskbuffername '(display-buffer-reuse-window))
+       (display-buffer (get-buffer-create taskbuffername) '(display-buffer-reuse-window))
        (setq lame-task ,taskname
              lame-task-buffer (get-buffer taskbuffername)
              lame-continue-callback nil)
-       (message "asdfasdf %s %s" lame-task-buffer taskbuffername)
        (with-current-buffer lame-task-buffer
          (setq buffer-read-only t)
          (let ((inhibit-read-only t))
@@ -58,6 +61,9 @@ SYMBOLS should be a list the length of the interactive args of FUNC.
        ,@body)))
 
 (defmacro lame/step (stepname description &rest body)
+  "A step with STEPNAME of a task.
+Add DESCRIPTION to the `lame-task-buffer'.
+Then execute BODY."
   `(progn
      (display-buffer lame-task-buffer '(display-buffer-reuse-window))
      (with-current-buffer lame-task-buffer
@@ -76,6 +82,7 @@ SYMBOLS should be a list the length of the interactive args of FUNC.
 
 (defun lame/done ()
   "Call when you are done with the current task."
+  (message "Task %s completed!" lame-task)
   (setq lame-task nil
         lame-continue-callback nil
         lame-task-buffer nil))
@@ -85,32 +92,96 @@ SYMBOLS should be a list the length of the interactive args of FUNC.
 Sets `lame-continue-callback' to evaluate AFTER."
   `(progn
      (setq lame-continue-callback
-           (lambda () ,@after))
+           (lambda () ,after))
      ,before))
 
 (defmacro lame/show-buffer (msg buffer &rest body)
-  "Show MESSAGE and show to BUFFER.
+  "Show MSG and show BUFFER.
 BODY will get executed when calling `lame/continue'."
-  `(progn
-     (switch-to-buffer ,buffer)
-     (lame/defer (message ,msg) ,body)))
+  `(lame/confirm
+    ,msg
+    (switch-to-buffer ,buffer)
+     ,@body))
 
-(defun blabla ()
-  "asdf."
-  (interactive)
-  (lame/task
-   "testrun"
-   "Try all the different macros to see how they behave."
-   (lame/step
-    "Show Buffer"
-    "Call lame/show-buffer. It should display msg and a buffer.
-Calling lame/continue will execute body."
-    (lame/show-buffer
-     "Interesting message"
-     "funcs.el"
-     (lame/step
-      "finished"
-      "Congrats!!!!!"
-      (lame/done))))))
+(defmacro lame/confirm (prompt before &rest after)
+  "Ask the user to continue with PROMPT after executing BEFORE.
+If the user agrees execute AFTER else set AFTER as `lame-continue-callback'."
+  `(progn
+     ,before
+     (if (y-or-n-p ,prompt)
+         (progn ,@after)
+       (setq lame-continue-callback
+             (lambda () ,@after)))))
+
+(defmacro lame/switch-branch (branch repo &rest after)
+  "Checkout BRANCH in a git REPO.
+Execute AFTER afterwards."
+  `(lame/confirm
+    (format "Checkout branch %s now?" ,branch)
+    (magit-status ,repo)
+    (magit-checkout ,branch)
+    ,@after))
+
+(defmacro lame/edit-text (msg var text mode &rest after)
+  "Show MSG and an edit buffer.
+Once the user calls `lame-continue' VAR is set with the current text of the buffer.
+TEXT is the initial text in the buffer.
+Execute AFTER afterwards."
+  `(lame/defer
+    (progn
+      (setq lame-edit-buffer (switch-to-buffer-other-window (format "EDIT_%s" ,var)))
+      (erase-buffer)
+      (insert ,text)
+      (when ,mode
+        (funcall ,mode))
+      (message ,msg))
+    (progn
+      (set ,var (buffer-string))
+      (kill-buffer-and-window)
+      (setq lame-edit-buffer nil)
+      ,@after)))
+
+(defun lame/add-text-to-description (text &optional wrap lang)
+  "Add TEXT to the description of the current task/step.
+If WRAP is non-nil wrap the text in a #+BEGIN_<wrap> #+END_<wrap> block.
+If LANG is non-nil add it like this:  #+BEGIN_<wrap> <lang>."
+  (with-current-buffer lame-task-buffer
+    (goto-char (point-max))
+    (let* ((inhibit-read-only t))
+      (if wrap
+          (insert
+           (format
+            "\n#+BEGIN_%s%s\n%s\n#+END_%s"
+            wrap (if lang (concat " " lang) "") text wrap))
+        (insert (concat "\n" text))))))
+
+(defun example-commit (version)
+  "Example commitng with a releasemessage."
+  (interactive "sVersion: ")
+  (eval
+    `(lame/task
+      (format "Release %s" ,version)
+      (format "Releasing version %s" ,version)
+      (lame/step
+       "Checkout master"
+       "Checkout master branch."
+       (lame/switch-branch
+        "master" "~/projects/emaci/"
+        (lame/step
+         "Update README.rst"
+         "Add release notes to readme."
+         (lame/edit-text
+          "Edit release notes!"
+          'releasenotes
+          (format "Releasenotes for version %s:\n%s" ,version (shell-command-to-string "git status"))
+          nil
+          (lame/add-text-to-description releasenotes "EXAMPLE" "python")
+          (lame/step
+           "Commit release notes"
+           "Use release notes as commit message."
+           (kill-new releasenotes)
+           (let ((default-directory "~/projects/emaci/"))
+             (magit-commit))
+           (lame/done)))))))))
 
 ;;; funcs.el ends here
