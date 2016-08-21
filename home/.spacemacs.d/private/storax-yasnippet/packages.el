c6b91101c34cafdcea8836406bf03ed2e6f65057
David Zuber <zuber.david@gmx.de>
2016-08-21 23:08:04 +0100

    Use spacemacs snippets as main snippets dir

---
+++ home/.spacemacs.d/private/storax-yasnippet/packages.el
@@ -0,0 +1,28 @@
+;;; packages.el --- storax-yasnippet layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: David Zuber <zuber.david@gmx.de>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defconst storax-yasnippet-packages
+  '(yasnippet))
+
+(defun storax-yasnippet/post-init-yasnippet ()
+  (use-package yasnippet
+    :config
+    (progn
+      ;; Put spacemacs snippets to the front
+      (let ((snippetdir "/home/david/.spacemacs.d/snippets"))
+        (setq yas-snippet-dirs (remove snippetdir yas-snippet-dirs))
+        (add-to-list 'yas-snippet-dirs snippetdir)))))
+
+;;; packages.el ends here
